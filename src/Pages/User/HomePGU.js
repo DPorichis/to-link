46d1b965b6d641604876a9091e0173dcef4e05c1
Ragkosgiorgos <100347566+Ragkosgiorgos@users.noreply.github.com>
2024-08-01 16:32:23 +0300

    side bar users home page

--- src/Pages/User/HomePGU.js
+++ src/Pages/User/HomePGU.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 import Header from "../../Components/Header";
+import './homePGU.css';
 
 function HomePGU(props) {
     return (
         <div>
             <Header log="user" act="home"/>
             I am a Page :D
+            <div className="sidebar">
+            Profile view
+                <div className="Profile">
+                    NAME NAME 
+                    MPLA MPLA
+                </div>
+                MY Links
+                <div className = "Links">
+                    <div className = "link">
+                        IOANNIC
+                    </div>
+                    <div className = "link">
+                        LAKIS
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
