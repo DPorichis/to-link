0c8b31ea9c8ffb747896b90e9628b291e44e1cb5
Ragkosgiorgos <100347566+Ragkosgiorgos@users.noreply.github.com>
2024-08-05 16:09:53 +0300

    messages update

--- src/Pages/User/MessagesPGU.js
+++ src/Pages/User/MessagesPGU.js
@@ -75,20 +75,42 @@ function MessagesPGU(props) {
     return (
         <div >
             <Header log="user" act="messages"/>
-            <div className="sidebar">
-                <div style={{ textAlign: "left" }}>
-                Your DMs
+            <div style={{display:"flex",flexDirection:"row"}}>
+                <div className="sidebar">
+                    <div style={{ textAlign: "left" }}>
+                    Your DMs
+                    </div>
+                    <div className = "LinksDms">
+                        {links.map((link) =>
+                        <ProfileDms name = {link.name} title = {link.title} Message={link.Message} imgURL={link.imgURL} user={link.User} Prev={link.Prev}/>
+                        )}
+                    </div>
                 </div>
-                <div className = "LinksDms">
-                {links.map((link) =>
-                <ProfileDms name = {link.name} title = {link.title} Message={link.Message} imgURL={link.imgURL} user={link.User} Prev={link.Prev}/>
-            )}
-            
+                <div className="Textbox" style={{display:"flex", flexDirection:"column",textAlign:"left",marginLeft:"60px",justifyContent:"space-between",width:"50%"}}>
+                    <p>
+                        <div style={{display:"flex", flexDirection:"row",textAlign:"left"}}>
+                            <h3 style={{fontSize:"32px",marginRight:"8px",marginBottom:"0px"}}>{links[1].name}</h3>
+                            <p style={{color:"#D3D3D3",fontSize:"20px",marginTop:"7px",marginBottom:"0px"}}>{links[1].title}</p>
+                        </div>
+                        <p style={{marginTop:"0px"}}>online</p>
+                        <div style={{display:"flex", flexDirection:"column",}}>
+                        <MessageCont messages = {links} />
+                        </div>
+                    </p>
+                    <p>
+                        <div className="ChatBox" style={{display:"flex",flexDirection:"row",width:"100%",height:"40px",borderRadius:"18px",backgroundColor: "#cecdcd"}}>
+                            <div style={{borderRadius:"18px",height:"100%",width:"100%"}}>
+                            <button style={{width:"10%",backgroundColor: "transparent",height:"90%",border:"none"}}>
+                            <img src="/plus-circle.svg" />
+                            </button>
+                            <input style={{width:"80%",backgroundColor: "transparent",height:"100%",border:"none",borderLeft:"solid"}} placeholder="Type a message" type="text"></input> 
+                            <button style={{width:"10%",backgroundColor: "transparent",border:"none"}}>
+                            <img src="/upload.svg" style={{}} />
+                            </button>
+                            </div>
+                        </div>
+                    </p>
                 </div>
-            
-            </div>
-            <div style={{display:"flex", flexDirection:"column",}}>
-            <MessageCont messages = {links} />
             </div>
         </div>
     );
