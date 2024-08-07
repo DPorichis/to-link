9abd59c189ccad7ee6d02dcf25964ab80aa87016
Ragkosgiorgos <100347566+Ragkosgiorgos@users.noreply.github.com>
2024-08-07 18:25:29 +0300

    Dismiss button

--- src/Components/Notifications/ProfileBannerClout.js
+++ src/Components/Notifications/ProfileBannerClout.js
@@ -1,8 +1,17 @@
 import React from "react";
-
+import { useState } from "react";
 import NetworkPGU from "../../Pages/User/NetworkPGU";
 
 function ProfileBannerClout(props){
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleDismiss = () => {
+        setIsVisible(false);
+    };
+
+    if (!isVisible) {
+        return null;
+    }
     return(
         
         <div className="Box" style={{backgroundColor: "#cecdcd",width:"100%",flexDirection:"row",padding:"5px 10px", display:"flex", justifyContent:"space-between", marginTop:"9px",marginBottom:"9px",borderRadius:"10px",textAlign: "center"}}>
@@ -13,7 +22,7 @@ function ProfileBannerClout(props){
                 </div>
             </div>
             <div style={{display: "flex",flexDirection: "row",alignItems: "center",justifyContent: "center"}}>
-                <button type="button" style={{padding: "5px 10px", cursor: "pointer", backgroundColor: "#cecdcd", color: "black", borderRadius: "5px",marginRight:"3px"}}>
+                <button type="button" onClick={handleDismiss} style={{padding: "5px 10px", cursor: "pointer", backgroundColor: "#cecdcd", color: "black", borderRadius: "5px",marginRight:"3px"}}>
                             Dismiss
                 </button>
             </div>
