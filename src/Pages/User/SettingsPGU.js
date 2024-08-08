2cadc5879db075c2912baab4bd0acea3db9d4cab
Dimitris Porichis <porichisdimitris@gmail.com>
2024-08-08 18:56:02 +0300

    settings midway done

--- src/Pages/User/SettingsPGU.js
+++ src/Pages/User/SettingsPGU.js
@@ -1,80 +1,307 @@
 import React, { useState } from "react";
 import Header from "../../Components/Header";
 
-const user = {
-    email: "something@gmail.com",
-    password: "1234"
+const dummyprofile = {
+    id: 12,
+    name: "Lakis",
+    surname: "Lalakis",
+    email: "kati@kati.cy",
+    phone: "+306947589234",
+    birthdate: "10/10/10",
+    country: "Greece",
+    city: "Athens",
+    password: "...",
 };
 
 function SettingsPGU(props) {
-    const [email, setEmail] = useState(user.email);
-    const [password, setPassword] = useState(user.password);
-    const [editEmail, setEditEmail] = useState(false);
-    const [editPassword, setEditPassword] = useState(false);
-    const [showPassword, setShowPassword] = useState(false);
-    const [iconState, setIconState] = useState('settings'); // New state for icon
+    const [savedProfile, setSavedProfile] = useState(dummyprofile);
+    const [editedProfile, setEditedProfile] = useState(savedProfile);
 
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
+    const [personalEdit, setPersonalEdit] = useState(false);
+    const [loginEdit, setLoginEdit] = useState(false);
+
+
+
+
+    const validateForm = () =>
+        {
+    
+        const errors = {};  
+    
+        if(editedProfile.name === "")
+        {
+            errors.name = 'Please enter your name.';
+        }
+    
+        if(editedProfile.surname === "")
+        {
+            errors.surname = 'Please enter your surname.';
+        }
+
+        // Regular expression for validating an email
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+        if (editedProfile.email === "" || !re.test(editedProfile.email))
+        {
+            errors.email = 'You must enter a valid email address'
+        }
+    
+        return errors;
+    
+    };
+    
+    const [formErrors, setFormErrors] = useState({
+        name: '',
+        surname: '',
+        email: '',
+    });
+
+
+    const handleFormChange = (e) => {
+        const { name, value} = e.target;
+        setEditedProfile({
+            ...editedProfile,
+            [name]: value,
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = validateForm();
+        setFormErrors(errors);
+        if (Object.keys(errors).length === 0) {
+            console.log('Form submitted successfully:', passwordForm);
+            setSavedProfile({
+                ...editedProfile,
+                password: savedProfile.password
+            });
+            alert("Password Updated successfully")
+        } else {
+            setFormErrors(errors);
+        }
     };
 
-    const toggleIcon = () => {
-        setIconState(iconState === 'settings' ? 'check' : 'settings');
+
+    const [passwordFormErrors, setPasswordFormErrors] = useState({
+        oldPassoword: '',
+        password: '',
+        passwordVal: '',
+    });
+
+    const [passwordForm, setPasswordForm] = useState({
+        oldPassoword: '',
+        password: '',
+        passwordVal: '',
+    });
+
+
+    const handlePasswordFormChange = (e) => {
+        const { name, value} = e.target;
+        setPasswordForm({
+            ...passwordForm,
+            [name]: value,
+        });
+    }
+
+    const handlePasswordChange = (e) => {
+        e.preventDefault();
+        const errors = validatePasswordForm();
+        setPasswordFormErrors(errors);
+        if (Object.keys(errors).length === 0) {
+            console.log('Form submitted successfully:', passwordForm);
+            setSavedProfile({
+                ...savedProfile,
+                password: passwordForm.password
+            });
+            setPersonalEdit(false);
+            alert("Profile Updated successfully");
+        } else {
+            setPasswordFormErrors(errors);
+        }
     };
+
+    const validatePasswordForm = () =>
+    {
+
+    const errors = {};  
+
+    if(passwordForm.oldPassoword !== savedProfile.password)
+    {
+        errors.oldPassoword = 'Wrong Password';
+    }
+
+    if (passwordForm.password !== passwordForm.passwordVal) {
+        errors.passwordVal = 'Passwords do not match';
+    }
+    
+    if (passwordForm.password === ""){
+        errors.password = 'You must enter a password'
+    }
+
+    return errors;
+
+    };
+
 
     return (
         <div>
             <Header log="user" act="settings" />
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                    <h4>Settings</h4>
-                    <img 
-                        src={`/${iconState}.svg`} 
-                        style={{ marginLeft: "3px", cursor: "pointer" }} 
-                        alt={iconState === 'settings' ? "Settings icon" : "Check icon"} 
-                        onClick={toggleIcon}
-                    />
-                </div>
-                <div style={{ padding: "20px", textAlign: "center", border: "1px solid black" }}>
-                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                        <h6 style={{ marginTop: "1%" }}>Email:</h6>
-                        {editEmail ? (
-                            <input 
-                                type="text" 
-                                value={email} 
-                                onChange={(e) => setEmail(e.target.value)} 
-                                style={{ marginLeft: "3px" }}
-                            />
-                        ) : (
-                            <p style={{ marginLeft: "3px" }}>{email}</p>
-                        )}
-                        <button style={{ border: "none" }} onClick={() => setEditEmail(!editEmail)}>
-                            <img src="/edit.svg" alt="Edit icon" />
-                        </button>
+            <div style={{ display: "flex", justifyContent: "left", alignItems: "left", flexDirection: "column", textAlign:"left",
+                marginLeft:"15%", width:"70%"
+            }}>
+                <h4>Settings</h4>
+                <div class="row" style={{marginBottom:"20px", padding: "10px 10px", borderRadius: "10px", border: "#ddd solid 1px"}}>
+                    <div style={{flexDirection:"row", justifyContent:"space-between", display:"flex"}}>
+                        <h5>Your Personal Information</h5>
+                            {personalEdit 
+                            ?
+                                <div style={{display:"flex", flexDirection:"row"}}>
+                                    <button class="btn btn-danger btn-sm" style={{marginRight:"3px"}}
+                                    onClick={() => {setEditedProfile(savedProfile); setPersonalEdit(false);}}>
+                                        Discard Changes
+                                    </button>                                
+                                    <button class="btn btn-primary btn-sm" onClick={handleSubmit}>
+                                        Save Changes
+                                    </button>
+                                </div>
+                            :
+                                <button class="btn btn-primary btn-sm" onClick={() => {setPersonalEdit(true)}}>
+                                    Edit
+                                </button>
+                        }
                     </div>
-                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                        <h6 style={{ marginTop: "1%" }}>Password:</h6>
-                        {editPassword ? (
-                            <input 
-                                type={showPassword ? "text" : "password"} 
-                                value={password} 
-                                onChange={(e) => setPassword(e.target.value)} 
-                                style={{ marginLeft: "3px" }}
-                            />
-                        ) : (
-                            <p style={{ marginLeft: "3px" }}>{showPassword ? password : "****"}</p>
-                        )}
-                        <button style={{ border: "none" }} onClick={() => setEditPassword(!editPassword)}>
-                            <img src="/edit.svg" alt="Edit icon" />
-                        </button>
-                        {editPassword && (
-                            <button style={{ border: "none" }} onClick={togglePasswordVisibility}>
-                                <img src={showPassword ? "/eye.svg" : "/eye-off.svg"} alt={showPassword ? "Hide" : "Show"} />
+                    <div class="col-md-12" style={{marginBottom:"5px"}}>
+                        <div class="row">
+                            <div className="col-md-3" style={{marginBottom:"5px"}}>
+                                <label htmlFor="name" className="form-label">Name</label>
+                                <input
+                                    type="text"
+                                    className={`form-control ${formErrors.name ? 'is-invalid' : ''}`}
+                                    id="name"
+                                    name="name"
+                                    value={editedProfile.name}
+                                    onChange={handleFormChange}
+                                    required
+                                    placeholder="Enter your name"
+                                    disabled={!personalEdit}
+                                />
+                                <div className="invalid-feedback">{formErrors.name || 'Please enter your name.'}</div>
+                            </div>
+                            <div className="col-md-3" style={{marginBottom:"5px"}}>
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input
+                                    type="text"
+                                    className={`form-control ${formErrors.email ? 'is-invalid' : ''}`}
+                                    id="email"
+                                    name="email"
+                                    value={editedProfile.email}
+                                    onChange={handleFormChange}
+                                    required
+                                    placeholder="Enter your email"
+                                    disabled={!personalEdit}
+                                />
+                                <div className="invalid-feedback">{formErrors.email || 'Please enter your email.'}</div>
+                            </div>
+                            <div class="col-md-3" style={{marginBottom:"5px"}}>
+                                <label class="form-label" style={{marginBottom:"2px"}}>Country</label>
+                                <input type="text" class="form-control" value={editedProfile.country} disabled={!personalEdit}
+                                onChange={handleFormChange} name="country"/>
+                            </div>
+                            <div class="col-md-3" style={{marginBottom:"5px"}}>
+                                <label class="form-label" style={{marginBottom:"2px"}}>Birthdate</label>
+                                <input type="text" class="form-control" value={editedProfile.birthdate} disabled={!personalEdit}
+                                onChange={handleFormChange} name="birthdate"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12" style={{marginBottom:"5px"}}>
+                        <div class="row">
+                            <div className="col-md-3" style={{marginBottom:"5px"}}>
+                                <label htmlFor="name" className="form-label">Surname</label>
+                                <input
+                                    type="text"
+                                    className={`form-control ${formErrors.surname ? 'is-invalid' : ''}`}
+                                    id="surname"
+                                    name="surname"
+                                    value={editedProfile.surname}
+                                    onChange={handleFormChange}
+                                    required
+                                    placeholder="Enter your surname"
+                                    disabled={!personalEdit}
+                                />
+                                <div className="invalid-feedback">{formErrors.surname || 'Please enter your surname.'}</div>
+                            </div>
+                            <div class="col-md-3" style={{marginBottom:"5px"}}>
+                                <label class="form-label" style={{marginBottom:"2px"}}>Phone</label>
+                                <input type="text" class="form-control" value={editedProfile.phone} disabled={!personalEdit}
+                                onChange={handleFormChange} name="phone"/>
+                            </div>
+                            <div class="col-md-3" style={{marginBottom:"5px"}}>
+                                <label class="form-label" style={{marginBottom:"2px"}}>City</label>
+                                <input type="text" class="form-control" value={editedProfile.city} disabled={!personalEdit}
+                                onChange={handleFormChange} name="city"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" style={{marginBottom:"20px", padding: "10px 10px", borderRadius: "10px", border: "#ddd solid 1px"}}>
+                    <h5>Update your password</h5>
+                    <form onSubmit={handlePasswordChange}>
+                        <div class="col-md-12" style={{marginBottom:"5px"}}>
+                            <div class="row">
+                                <div className="col-md-3" style={{marginBottom:"5px"}}>
+                                    <label htmlFor="password" className="form-label">Current Password</label>
+                                    <input
+                                        type="password"
+                                        className={`form-control ${passwordFormErrors.oldPassoword ? 'is-invalid' : ''}`}
+                                        id="oldPassoword"
+                                        name="oldPassoword"
+                                        value={passwordForm.oldPassoword}
+                                        onChange={handlePasswordFormChange}
+                                        required
+                                        placeholder="Enter your password"
+                                    />
+                                    <div className="invalid-feedback">{passwordFormErrors.password || 'Wrong Password.'}</div>
+                                </div>
+                                <div className="col-md-3" style={{marginBottom:"5px"}}>
+                                    <label htmlFor="password" className="form-label">New Password</label>
+                                    <input
+                                        type="password"
+                                        className={`form-control ${passwordFormErrors.password ? 'is-invalid' : ''}`}
+                                        id="password"
+                                        name="password"
+                                        value={passwordForm.password}
+                                        onChange={handlePasswordFormChange}
+                                        required
+                                        placeholder="Enter your password"
+                                    />
+                                    <div className="invalid-feedback">{passwordFormErrors.password || 'Please enter a password.'}</div>
+                                </div>
+                                <div className="col-md-3" style={{marginBottom:"5px"}}>
+                                    <label htmlFor="password" className="form-label">New Password Validation</label>
+                                    <input
+                                        type="password"
+                                        className={`form-control ${passwordFormErrors.passwordVal ? 'is-invalid' : ''}`}
+                                        id="passwordVal"
+                                        name="passwordVal"
+                                        value={passwordForm.passwordVal}
+                                        onChange={handlePasswordFormChange}
+                                        required
+                                        placeholder="Enter your password"
+                                    />
+                                    <div className="invalid-feedback">{passwordFormErrors.passwordVal || 'Please enter a password.'}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style={{display:"flex", flexDirection:"row", justifyContent:"right"}}>
+                            <button class="btn btn-primary">
+                                Change Password
                             </button>
-                        )}
-                    </div>
+                        </div>
+                    </form>
+                </div>            
                 </div>
-            </div>
         </div>
     );
 }
