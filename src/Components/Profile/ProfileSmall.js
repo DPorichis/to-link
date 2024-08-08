adb658d70259e56ba1d6d4ca04646a550aa13e89
Ragkosgiorgos <100347566+Ragkosgiorgos@users.noreply.github.com>
2024-08-08 18:12:31 +0300

    Hover

--- src/Components/Profile/ProfileSmall.js
+++ src/Components/Profile/ProfileSmall.js
@@ -7,11 +7,13 @@ function ProfileSmall(props){
 
 
 return(
-<li className = "link" key={props.name}>
-                            <img src={props.imgURL} alt="Avatar" style={{width :"15%"}} className="link-image" />
-                            <span className="link-name">{props.name}</span>
-                            <span className="link-title">{props.title}</span>
-                        </li>
+<li className = "link"  key={props.name}>
+    <img src={props.imgURL} alt="Avatar" style={{width :"50px",height:"50px",marginRight: '2px'}} className="link-image" />
+    <div style={{display:"flex",flexDirection:"column"}}>
+        <div className="link-name" style={{textAlign:"left", margin: '0'}}>{props.name}</div>
+        <div className="link-title" style={{textAlign:"left", margin: '0'}}>{props.title}</div>
+    </div>
+</li>
 );
 }
 export default ProfileSmall
